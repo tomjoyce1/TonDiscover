fa430f8d403c20eadabb68ffe8ed218d15d29459
tomjoyce123 <thomasjoyceofficial@gmail.com>
2026-03-21 16:47:30 +0000

    recip wallet address

---
+++ src/constants/common-constants.ts
@@ -0,0 +1,4 @@
+import { Address } from '@ton/core';
+
+export const USDT_MASTER_ADDRESS = Address.parse('kQD0GKBM8ZbryVk2aESmzfU6b9b_8era_IkvBSELujFZPsyy');
+export const INVOICE_WALLET_ADDRESS = Address.parse('0QBdA277paC1z5kGns6CToio7P__MquQMcrErQaEldzvcnPk');
